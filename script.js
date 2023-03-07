e7cfbb9cb62d25269fa411ba25c47875d51857f9
sylph <124384687+sylph88@users.noreply.github.com>
2023-03-06 21:21:41 -0500

    add color picker and reset button

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const ROW_SIZE = 16;
 const COLUMN_SIZE = 16;
-const appDiv = document.createElement("div");
-appDiv.classList.add("square");
 
-const container = document.querySelector(".app-container")
+const appDiv = document.createElement("div");
+const color = document.querySelector("#pen-color")
+const container = document.querySelector(".sketch-container")
+const resetButton = document.querySelector('button[name="reset"]');
+
+appDiv.classList.add("square");
 
 for (let i = 0; i < ROW_SIZE * COLUMN_SIZE; i++) {
     let clone = appDiv.cloneNode(false);
@@ -14,7 +17,13 @@ const squares = document.querySelectorAll(".square");
 
 squares.forEach((x) => {
     x.addEventListener("mouseover", () => {
-        x.style.backgroundColor = "red";
+        x.style.backgroundColor = color.value;
     });
+});
+
+resetButton.addEventListener("click", () => {
+    squares.forEach((x) => {
+        x.style.backgroundColor = "#ffffff";
+    })
 });
 
